f7e12d1ee99d9177d4d067765d1bf50f4f416610
Shaun Hess <shaunhess@gmail.com>
2018-11-01 13:21:34 -0400

    Day 0

---
+++ Day_0/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	inputString, _ := reader.ReadString('\n')
+	fmt.Println("Hello, World.")
+	fmt.Println(inputString)
+}
